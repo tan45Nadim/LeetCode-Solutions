5160ca8b0b4555f163bfc8c7a3c7d8cc0e7f06f1
Tanjir Ahmed Nadim <106719777+tan45Nadim@users.noreply.github.com>
2023-03-18 13:02:07 +0600

    Create 1757_RecyclableandLowFatProducts.sql

---
+++ Database/1757_RecyclableandLowFatProducts.sql
@@ -0,0 +1,6 @@
+select products.product_id
+from products
+where products.low_fats = 'Y' and products.recyclable = 'Y'
+
+
+-- https://leetcode.com/problems/recyclable-and-low-fat-products/submissions/916637998/

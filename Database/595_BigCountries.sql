dd8cf58ac77d8c8f793c6262f1f26a3afcacbe05
Tanjir Ahmed Nadim <106719777+tan45Nadim@users.noreply.github.com>
2023-03-21 23:27:59 +0600

    Create 595_BigCountries.sql

---
+++ Database/595_BigCountries.sql
@@ -0,0 +1,6 @@
+select world.name, world.population, world.area
+from world
+where world.area >= 3000000 or world.population >= 25000000
+
+
+-- https://leetcode.com/problems/big-countries/submissions/919640310/
